de3af8a20a7783a3f5a2412002c9181a5a7faa5a
xt4k <yuriy.logvinov@gmail.com>
2019-03-22 01:24:46 +0200

    14 LESSON      21-mar-2019      00 40

--- src/test/java/com.academy/automationpractice/TestConfReader.java
+++ src/test/java/com.academy/automationpractice/TestConfReader.java
@@ -10,6 +10,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestConfReader {
@@ -42,19 +43,25 @@ public class TestConfReader {
 
     public TestConfReader(String confFilePathFileName, String sheetName, int rowNum) {
         File file = new File( confFilePathFileName );
-        //XSSFWorkbook workbook;
+        List<String> url = new ArrayList<>();
+        List<String> login = new ArrayList<>();
+        List<String> password = new ArrayList<>();
+        List<String> expectedErrMsg = new ArrayList<>();
+
+        // XSSFWorkbook workbook;
         HSSFWorkbook workbook;
         try {
-            workbook = new HSSFWorkbook( new FileInputStream( file ) );// XSSFWorkbook( new FileInputStream( file ) );
+            //workbook = new  XSSFWorkbook( new FileInputStream( file ) );
+            workbook = new HSSFWorkbook( new FileInputStream( file ) );
             HSSFSheet sheet = workbook.getSheet( sheetName );
             //XSSFSheet sheet = workbook.getSheet( sheetName );
-            for (int i = 1; i <= rowNum; i++) {
-                HSSFRow parRow = sheet.getRow( row );
+            for (int i = 2; i <= rowNum; i++) {
+                HSSFRow parRow = sheet.getRow( i );
                 // XSSFRow parRow = sheet.getRow( row );
-                this.url.add( parRow.getCell( 0 ).getStringCellValue() );
-                this.login.add( parRow.getCell( 1 ).getStringCellValue() );
-                this.password.add( parRow.getCell( 2 ).getStringCellValue() );
-                this.expectedErrMsg.add( parRow.getCell( 3 ).getStringCellValue() );
+                url.add( parRow.getCell( 0 ).getStringCellValue() );
+                login.add( parRow.getCell( 1 ).getStringCellValue() );
+                password.add( parRow.getCell( 2 ).getStringCellValue() );
+                expectedErrMsg.add( parRow.getCell( 3 ).getStringCellValue() );
             }
         } catch (IOException e) {
             System.out.println( "Something wrong" );
